7df16494a4806f4c2f0fe014ce0afe0d621360a2
emmcgill <mcgill.erik@gmail.com>
2020-05-25 20:03:19 -0400

    Initial Commit

---
+++ Data/Park.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Park
+    {
+        [Key]
+        public int ParkId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string ParkDescription { get; set; }
+
+    }
+}
---
+++ Data/Trail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Trail
+    {
+        [Key]
+        public int TrailId { get; set; }
+
+        [ForeignKey(nameof(Park))]
+        public int ParkId { get; set; }
+        public virtual Park Park { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Length { get; set; }
+        [Required]
+        public string TrailType { get; set; }
+    }
+}
